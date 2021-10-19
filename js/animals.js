accc254f208cf1f22927041cbf3a06baeb71bf99
FesalBadday <fesal.n04@hotmail.com>
2021-10-19 12:13:03 -0600

    updated some text

--- js/animals.js
+++ js/animals.js
@@ -2,86 +2,86 @@
 export const picsumAnimals = [
   {
     id: 237,
-    title: 'Puppy',
+    title: 'Cute Puppy Photo',
     src: '237.jpg',
-    alt: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.'
+    alt: 'Puppy Photo.'
   },
   {
     id: 433,
     title: 'Bear',
     src: '433.jpg',
-    alt: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.'
+    alt: 'Bear Photo.'
   },
   {
     id: 577,
     title: 'Moose',
     src: '577.jpg',
-    alt: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.'
+    alt: 'Moose Photo.'
   },
   {
     id: 582,
     title: 'Coyote',
     src: '582.jpg',
-    alt: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.'
+    alt: 'Coyote Photo.'
   },
   {
     id: 593,
     title: 'Tiger',
     src: '593.jpg',
-    alt: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.'
+    alt: 'Tiger Photo.'
   },
   {
     id: 659,
     title: 'Husky',
     src: '659.jpg',
-    alt: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.'
+    alt: 'Husky Photo.'
   },
   {
     id: 718,
     title: 'Wolf',
     src: '718.jpg',
-    alt: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.'
+    alt: 'Wolf Photo.'
   },
   {
     id: 783,
     title: 'Monkey',
     src: '783.jpg',
-    alt: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.'
+    alt: 'Monkey Photo.'
   },
   {
     id: 790,
     title: 'Elk',
     src: '790.jpg',
-    alt: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.'
+    alt: 'Elk Photo.'
   },
   {
     id: 837,
     title: 'Bulldog',
     src: '837.jpg',
-    alt: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.'
+    alt: 'Bulldog Photo.'
   },
   {
     id: 1024,
     title: 'Bird',
     src: '1024.jpg',
-    alt: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.'
+    alt: 'Bird Photo.'
   },
   {
     id: 1025,
     title: 'Pug',
     src: '1025.jpg',
-    alt: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.'
+    alt: 'Pug Photo.'
   },
   {
     id: 1074,
     title: 'Big Cat',
     src: '1074.jpg',
-    alt: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.'
+    alt: 'Big Cat Photo.'
   },
   {
     id: 1084,
     title: 'Walrus',
     src: '1084.jpg',
-    alt: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.'
+    alt: 'Walrus Photo.'
   }
 ];
